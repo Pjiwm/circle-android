b69ec4eabed8c7278d16ba5f90ad5e95192a10d9
JopRillos2001 <47351860+JopRillos2001@users.noreply.github.com>
2022-06-17 10:53:23 +0100

    PublicKeysList

---
+++ app/src/main/java/com/pedro/rtpstreamer/utils/PublicKeysData.java
@@ -0,0 +1,48 @@
+package com.pedro.rtpstreamer.utils;
+
+public class PublicKeysData {
+    public PublicKeysClass[] getPublicKeys() {
+        PublicKeysClass[] publicKeys = new PublicKeysClass[] {
+                new PublicKeysClass(
+                        "Jop",
+                        1,
+                        "-----BEGIN PUBLIC KEY-----\n" +
+                                "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAruztFasZTIJ9HttjRwi7\n" +
+                                "2fprEG6J8mcxE7zyrMZQwiVUGHe20x1VZ2ukl2tvXO1lcsAFf12pdQKgK0PxQmUz\n" +
+                                "XYwU0C/+eV3F5ZUUb/DNlvRyGNys9wGsNcsUHYbimwz2TecQLacXGgAQlGRqorR5\n" +
+                                "8Vw02g8Hi0sqYpAPNPvaOroCTqCAGSfMl/+aLGdWEmv3LY6p82KW+gMbjyJwNF9Q\n" +
+                                "i8ZWHq0FivxbJKqpkkCqbLapCq6mEb7Pc3ZNOERwD5boUcqpRJIs9nMw3LUXkHeQ\n" +
+                                "V796oC9P94sFUY0B+9DkK6v4lEwAYeSGkduxNccWSBL62Zmf+RD977G1a2P1ED+m\n" +
+                                "NwIDAQAB\n" +
+                                "-----END PUBLIC KEY-----\n"
+                ),
+                new PublicKeysClass(
+                        "Diego",
+                        2,
+                        "-----BEGIN PUBLIC KEY-----\n" +
+                                "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0CqcW6AW9C2syT2otsgk\n" +
+                                "atFJi3RNt8Vga+DrgO5B9yzhI6foGfxzWpdwcgJ/hI4r+QVjTrqCqTrXeqOHIcQc\n" +
+                                "uSCpECPfkPCi2eGtVkp2QILI9gQre9H23aKOVUzrgZ8BNMm5rRgDtqL/tVUjCJyu\n" +
+                                "SEw3v7aLBQBsycpz4EUYxxnPrqLAdni3AwDUbE6n15IwkXrHYLqySnavxplqPrPg\n" +
+                                "CfPDkJKggq9b8UeiPUfIcglBuGMnUKIQys0CQTo74qU6WSUYfMVTMX51N5an1w0c\n" +
+                                "BB+QeYcArLYvwf5MowaguVvI0588nvSvghmu2pjMgt2vfkZ+lgrVwTEQY6+MwMG8\n" +
+                                "wwIDAQAB\n" +
+                                "-----END PUBLIC KEY-----\n"
+                ),
+                new PublicKeysClass(
+                        "Twan",
+                        3,
+                        "-----BEGIN PUBLIC KEY-----\n" +
+                                "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0Jry/VVG27X89+3mohBr\n" +
+                                "JyjN5ZciLvB3kauuxYs0dhrxnoOT9/1u+hYkf9nTYd8TDGB1zKXdpfMM2jENeoA3\n" +
+                                "fJd2jhSJYEj0o7sTP5lBuHoq3pBj0K5pbTk+fwAi2ek0T+TmA6KFl+aCXlgnsHns\n" +
+                                "W9jqK94G4QTvSRjVHwQ57uGukmC+Nn6wqSMnKLCyExqhOKycI0OJJ13gDntNXUxo\n" +
+                                "o4Bum0vUJyBQ+4cez0HGaK7kfZ/Yrp8s0FR5yhs40gt9A6heOKjiuQtTSCXCXx5R\n" +
+                                "69HyplG9qT8+GRVXOJdGwFn51iqXy0UBxCMvoV34ZXGxzGVCPJah2n8Pq8e2pJ7j\n" +
+                                "ywIDAQAB\n" +
+                                "-----END PUBLIC KEY-----\n"
+                )
+        };
+        return publicKeys;
+    }
+}
